4d0d0c1f3a776144c48f191b8326cb01f869c056
Joshua Morony <joshua.morony@gmail.com>
2024-02-02 22:40:07 +1030

    chatgpt

---
+++ lua/plugins/chatgpt.lua
@@ -0,0 +1,15 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      api_key_cmd = "op read op://private/OpenAI/credential --no-newline",
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+}

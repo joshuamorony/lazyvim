bd983b85b92fe682181a9000abbfa12fc26b6047
Joshua Morony <joshua.morony@gmail.com>
2024-02-03 13:32:59 +1030

    comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,6 +2,12 @@ return {
   "numToStr/Comment.nvim",
   opts = {
     -- add any options here
+    toggler = {
+      ---Line-comment toggle keymap
+      line = "<leader>cc",
+      ---Block-comment toggle keymap
+      block = "<leader>bc",
+    },
   },
   lazy = false,
 }

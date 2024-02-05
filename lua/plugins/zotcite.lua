213c4433d65f13bc0c8bc0e7c6b699cf46c5d121
Joshua Morony <joshua.morony@gmail.com>
2024-02-05 16:18:47 +1030

    updates

--- lua/plugins/zotcite.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "jalvesaq/zotcite",
-  config = function()
-    -- filetypes = { "pandoc", "markdown", "rmd", "r", "quarto" },
-    vim.g.zotcite_conceallevel = 0
-  end,
-  dependencies = "jalvesaq/cmp-zotcite",
-}

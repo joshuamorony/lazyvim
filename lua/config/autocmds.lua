9aab38719e41710a3bb40581ca9590876b2607b5
Joshua Morony <joshua.morony@gmail.com>
2024-02-05 16:23:51 +1030

    dont conceal code fences

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = { "*.md", "*.json", "*.mdx" },
+  callback = function()
+    vim.cmd("set conceallevel=0")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
+  pattern = { "*.md", "*.json", "*.mdx" },
+  callback = function()
+    vim.cmd("set conceallevel=3")
+  end,
+})

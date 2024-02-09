c65397af4a825e3c3f672ea878532f7f9f4a6a80
Joshua Morony <joshua.morony@gmail.com>
2024-02-09 14:34:03 +1030

    update obsidian

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -21,5 +21,19 @@ return {
         path = "~/Documents/notes",
       },
     },
+    note_id_func = function(title)
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = tostring(os.time()) .. "-" .. string.char(math.random(65, 90))
+        end
+      end
+      return suffix
+    end,
+    disable_frontmatter = true,
   },
 }

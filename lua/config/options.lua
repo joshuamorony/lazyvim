3ccbef01fdcb1e91cd3f1919c30ba4312c8fea34
Joshua Morony <joshua.morony@gmail.com>
2024-06-29 14:40:50 +0930

    change grep root dir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -75,3 +75,6 @@ vim.o.foldcolumn = "1" -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+-- change root dir for grep
+vim.g.root_spec = { "cwd" }

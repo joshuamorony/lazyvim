3832c1696cd9337abda9e13d40918874f68996c1
Joshua Morony <joshua.morony@gmail.com>
2024-07-21 21:42:05 +0930

    add ngt html lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -88,6 +88,41 @@ return {
         },
       },
       setup = {
+        html = function(_, opts)
+          opts.init_options = {
+            dataPaths = {
+              vim.fn.getcwd() .. "/node_modules/angular-three/metadata.json",
+            },
+            configurationSection = { "html", "css", "javascript" },
+            embeddedLanguages = {
+              css = true,
+              javascript = true,
+            },
+            provideFormatter = true,
+          }
+
+          opts.handlers = {
+            ["html/customDataContent"] = function(err, result, ctx, config)
+              local function exists(name)
+                if type(name) ~= "string" then
+                  return false
+                end
+                return os.execute("test -e " .. name)
+              end
+
+              if not vim.tbl_isempty(result) and #result == 1 then
+                if not exists(result[1]) then
+                  return ""
+                end
+                local content = vim.fn.join(vim.fn.readfile(result[1]), "\n")
+                return content
+              end
+              return ""
+            end,
+          }
+
+          return false
+        end,
         volar = function(_, opts)
           opts.filetypes = { "vue", "agx" }
         end,
